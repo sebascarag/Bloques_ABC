dfe48166e183b92eec342f3eee64d2e2ff9177e6
sebascarag <sebastiansca10@gmail.com>
2021-10-08 13:48:20 -0500

    ajuste para capturar letra a partir de la cara 1 del bloque y en caso de no encontrar bucar en la cara 2

--- src/bloquesabc/BloquesABC.java
+++ src/bloquesabc/BloquesABC.java
@@ -10,11 +10,13 @@ public class BloquesABC {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         char [] blocksABC1 = {'B', 'X', 'D', 'C', 'N', 'G', 'R', 'T', 'Q', 'F', 'H', 'V', 'A', 'O', 'E', 'F', 'L', 'P', 'Z', 'J'};
-        char [] blocksABC2 = {'O', 'K', 'Q', 'P', 'A', 'T', 'E', 'G', 'D', 'S', 'U', 'I', 'N', 'B', 'R', 'S', 'Y', 'C', 'M', 'W'};
+        char [] blocksABC2 = {'O', 'K', 'Q', 'P', 'A', 'T', 'E', 'G', 'D', 'S', 'U', 'I', 'T', 'B', 'R', 'S', 'Y', 'C', 'M', 'W'};
         boolean[] blockUsed = new boolean[20];
         boolean flagOk = false;
         char[] word = sc.next().toUpperCase().toCharArray();
         for (char w : word) {
+            /*
+            --- código presentado en la entrevista ---
             for (int i = 0; i < blocksABC1.length; i++) {
                 if ( (blocksABC1[i] == w || blocksABC2[i] == w) && !blockUsed[i]) {
                     blockUsed[i] = true;
@@ -22,6 +24,28 @@ public class BloquesABC {
                     break;
                 }else{
                     flagOk = false;
+                }
+            }*/
+            /* --- ajuste realizado para aceptar la palabra arlequin --- */
+            for (int i = 0; i < blocksABC1.length; i++) {
+                if ( blocksABC1[i] == w && !blockUsed[i]) {
+                    blockUsed[i] = true;
+                    flagOk = true;
+                    break;
+                }else{
+                    flagOk = false;
+                }
+            }
+            if (!flagOk) {
+                for (int i = 0; i < blocksABC2.length; i++) {
+                    if (blocksABC2[i] == w && !blockUsed[i]) {
+                        blockUsed[i] = true;
+                        flagOk = true;
+                        break;
+                    }
+                    else{
+                        flagOk = false;
+                    }
                 }
             }
         }
